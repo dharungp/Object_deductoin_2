154b3e9d89c118a224b0a82fcd1b64d1f716e2f3
dharungp <99897041+dharungp@users.noreply.github.com>
2024-04-30 12:35:49 +0530

    Add files via upload

---
+++ TvAc.js
@@ -0,0 +1,36 @@
+img=""
+status_of_the_cocossd_model=""
+function preload(){
+    
+    img = loadImage("tvac.jpg");
+}
+
+function Back(){
+    window.location="index.html"
+}
+
+
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+
+    }
+function draw(){
+    image(img,0,0,500,500)
+    }
+
+    
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status_of_the_cocossd_model = true;
+    objectDetector.detect(img, gotResults);
+    
+    }
+function gotResults(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+    objects = results;
+    }
---
+++ flower.js
@@ -0,0 +1,33 @@
+img="";
+status_of_the_cocossd_model=""
+function Back(){
+    window.location="index.html"
+}
+function preload(){
+img = loadImage("flower.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+
+    }
+function draw(){
+    image(img,0,0,500,500)
+    }
+
+    
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status_of_the_cocossd_model = true;
+    objectDetector.detect(img, gotResults);
+    
+    }
+function gotResults(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+    objects = results;
+    }
